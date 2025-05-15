7e7ea4020f1839144aea0683bebc17c4fbb11b7d
mokshilthegreat <kakusharma11111111@gmail.com>
2025-05-15 10:29:19 +0530

    question (2 are not done )

---
+++ day-5/index.js
@@ -0,0 +1,143 @@
+//Q1:-
+
+let radius = 7;
+
+let area = 22/7 * radius * radius;
+
+console.log(" Q1:- The area of the circle is:", area);
+
+//Q2:-
+
+let a = 10;
+let b = 20; 
+let c = 5;
+
+console.log(" Q2:-", a*b / c + b-a);
+
+//Q3:- 
+
+let x = 5;
+let y = 10;
+x += 2;
+y *= x;
+
+console.log(" Q3:- x=",x);
+console.log("      y=",y);
+
+// Q4:-
+let num = 10;
+let num2 = 10;
+let num3 = 10;
+let num4 = 10;
+ 
+num +=15;
+num2 -=5;
+num3 *=3 
+num4 /= 2
+
+console.log(" Q4:-",num);
+console.log(" (ii)=",num2);
+console.log("(iii)=",num3);
+console.log(" (iv)=",num4);
+
+//Q5:-
+
+let score1 = 85;
+let score2 = 90;
+
+console.log( "Q5:-",score1 > 80 && score2 > 80)
+
+//Q6:-
+
+let str1 = "apple";
+let str2 = "banana";
+
+let result = str1 < str2;
+
+console.log("Q6:-", result);
+
+//Q7:-
+
+let age = 15
+let hasDrivingLicense = true;
+
+console.log("Q7:-",age>=18 && hasDrivingLicense  )
+
+//Q8:-
+let Num1 = 10
+let Num2 = 50
+
+
+console.log("Q8:-",10>11 && 50<10)
+
+//Q9:-
+
+number = "123.45"
+number = Number(number)
+let num1 =number + 10;
+console.log( "Q9:-",num1);
+
+
+//Q10:-
+let A =true;
+A = String(A)
+let B = " is the value";
+console.log( "Q10:-",A+B);
+
+//Q11:-
+/* it's in innerhtml */
+
+//Q12:-
+
+let a1 = 5;
+let b1 = 10;
+let res = a1+b1;
+console.log( "Q12:-",res)
+
+//Q13:-
+
+let time = new Date()
+
+let hour = time.getHours()
+
+let minutes = time.getMinutes()
+
+let seconds = time.getSeconds()
+
+console.log("Q13:-",`${hour}:${minutes}:${seconds}`)
+
+//Q14:-
+
+
+
+
+
+
+//Q15:-
+
+
+let date = new Date()
+
+
+let day = date.getDate()
+
+let month = date.getMonth()+1
+
+let year = date.getFullYear()
+
+console.log("Q15:-",`${day}-${month}-${year}`)
+console.log(`      ${day}\\${month}\\${year}`)
+
+console.log(`      ${month}-${day}-${year}`)
+console.log(`      ${month}\\${day}\\${year}`)
+
+//Q16:-
+
+/*let side1 = 5;
+let side2 = 6;
+let side3 = 7;
+
+let Area = side1 + side2 + side3 /2;
+
+console.log("Q16:-",area )*/
+
